f445e9024a082beb5f796993fecafb7a06210745
Vasco Silva <100089965+reiid00@users.noreply.github.com>
2023-04-21 18:54:52 +0100

    Created Functions to visualize Attention Weights

--- Collection/Models/Transformer_from_scratch/utils/visualize.py
+++ Collection/Models/Transformer_from_scratch/utils/visualize.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import torch
+from transformer_v2 import Transformer
+from utils.function_utils import preprocess_text, generate_masks
 
 def plot_positional_encodings(encodings, outfile=None):
     plt.figure(figsize=(10, 10))
@@ -10,3 +13,34 @@ def plot_positional_encodings(encodings, outfile=None):
     if outfile is not None:
         plt.savefig(outfile)
     plt.show()
+
+def plot_attention_weights(attention, layer, head, outfile=None):
+    attention_data = attention[layer][0, head].cpu().detach().numpy()
+
+    plt.figure(figsize=(8, 8))
+    plt.imshow(attention_data, cmap='viridis', aspect='auto')
+    plt.colorbar()
+    plt.title(f"Attention Weights (Layer: {layer}, Head: {head})")
+    plt.xlabel("Key Positions")
+    plt.ylabel("Query Positions")
+    if outfile is not None:
+        plt.savefig(outfile)
+    plt.show()
+
+def view_attention_weights(model, src_text, tgt_text):
+
+    src = preprocess_text(src_text)
+    tgt = preprocess_text(tgt_text)
+    src_mask, tgt_mask = generate_masks(src, tgt)
+    with torch.no_grad():
+        _, enc_attention_weights, dec_self_attention_weights, dec_enc_attention_weights = model(src, tgt, src_mask, tgt_mask, return_attention=True)
+    layer = 2
+    head = 1
+    outfile = f"enc_attention_weights_layer{layer}_head{head}.png"
+    plot_attention_weights(enc_attention_weights, layer, head, outfile)
+
+    outfile = f"dec_self_attention_weights_layer{layer}_head{head}.png"
+    plot_attention_weights(dec_self_attention_weights, layer, head, outfile)
+
+    outfile = f"dec_enc_attention_weights_layer{layer}_head{head}.png"
+    plot_attention_weights(dec_enc_attention_weights, layer, head, outfile)
